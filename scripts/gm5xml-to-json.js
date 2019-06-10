1efcc2c16a57018cd2baddfc2292c6f17ca7f19d
Curtis Oakley <coakley@insidesales.com>
2019-06-10 15:36:25 -0600

    Add a basic script for converting GM5 xml to json

---
+++ scripts/gm5xml-to-json.js
@@ -0,0 +1,198 @@
+/*!
+This converts Game Master 5 formatted xml files to JSON.
+ */
+"use strict";
+
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const parseString = Promise.promisify(require('xml2js').parseString);
+const util = require('util');
+
+// TODO get arg for the file
+// TODO allow for a second arg to be specified that appends to an existing source file
+fs.readFileAsync(__dirname + "/../source/Volo's Guide to Monsters.xml", 'utf8')
+    .then(function(contents) {
+        // Convert the file contents from XML to JSON
+        return parseString(contents);
+    })
+    .then(function(compendium) {
+        // Get the compendium
+        if (!('compendium' in compendium)) {
+            throw 'Invalid compendium';
+        }
+        compendium = compendium.compendium;
+        // console.log('Processing compendium. Version:', compendium['$'].version);
+
+        // Find the spells and monsters in the compendium
+        let monsters = [],
+            spells = [];
+
+        if ('monster' in compendium) {
+            // Process monsters
+            compendium.monster.forEach(function(monster) {
+                monsters.push(processMonster(monster));
+            });
+        }
+        if ('spell' in compendium) {
+            // Process spells
+            // TODO
+        }
+
+        console.log(JSON.stringify(monsters));
+    })
+    .catch(function(err) {
+        console.log('Error:', err);
+    });
+
+function processTraits(value) {
+    let trait,
+        traits = [];
+
+    value.forEach(function (v) {
+        trait = {};
+        if ('name' in v && v.name.length > 0) {
+            trait['name'] = v.name[0].trim();
+        }
+        if ('text' in v && v.text.length > 0) {
+            trait['text'] = trimAll(v.text);
+        }
+        if ('attack' in v && v.attack.length > 0) {
+            if (v.attack.length > 1) {
+                trait['attack'] = trimAll(v.attack);
+            } else {
+                // Collapse to a single attack
+                trait['attack'] = v.attack[0].trim();
+            }
+        }
+        traits.push(trait);
+    });
+
+    return traits;
+}
+
+function trimAll(arr) {
+    return arr.map(function (v) {
+        return v.trim();
+    });
+}
+
+function csvSplit(str, isNumeric) {
+    return str.split(',')
+        .map(function (v) {
+            v = v.trim();
+            if (isNumeric) {
+                // Convert to an integer
+                v = parseInt(v, 10);
+                if (!validNumber(v)) {
+                    v = null;
+                }
+            }
+            return v;
+        })
+        .filter(function (v) {
+            return v !== null;
+        });
+}
+
+function validNumber(num) {
+    return !isNaN(num) && isFinite(num) && num >= 0
+}
+
+function parseIntSafe(value) {
+    if (typeof value === 'number') {
+        return value;
+    }
+
+    // Make sure we only have characters that can be a number
+    if (/[^0-9\-.]/.test(value)) {
+        return value;
+    }
+    let parsed = parseInt(value, 10);
+    if (isNaN(parsed) || !isFinite(parsed)) {
+        return value;
+    }
+    return parsed;
+}
+
+const legendaryActionRegex = /Legendary Actions \((\d+)\/Turn\)/;
+
+function processMonster(monster) {
+    let m = {},
+        value,
+        result;
+
+    for (var i in monster) {
+        if (monster.hasOwnProperty(i)) {
+            value = monster[i];
+            if (value.length > 0) {
+                switch (i) {
+                    // Traits and Abilities
+                    case 'trait':// Fallthrough
+                    case 'action':
+                    case 'reaction':
+                        // Convert the traits and save
+                        m[i] = processTraits(value);
+                        break;
+
+                    case 'legendary':
+                        // Extract the number of uses and the name from the text
+                        if ('name' in value[0] && (result = legendaryActionRegex.exec(value[0].name))) {
+                            m[i] = {
+                                "numUses": parseIntSafe(result[1]),
+                                "actions": processTraits(value.slice(1)),
+                            };
+                        } else {
+                            console.log('Invalid legendary actions', monster.name);
+                        }
+                        break;
+
+                    // This is a comma separated list
+                    case 'slots':// Fallthrough
+                    case 'spells':
+                        if (value[0].length > 0) {
+                            value = csvSplit(value[0], i === 'slots');
+
+                            if (value.length > 0) {
+                                m[i] = value;
+                            }
+                        }
+                        break;
+
+                    // These should be numbers
+                    case 'str':// Fallthrough
+                    case 'dex':
+                    case 'con':
+                    case 'int':
+                    case 'wis':
+                    case 'cha':
+                    case 'passive':
+                        value = parseInt(value[0], 10);
+                        if (validNumber(value)) {
+                            m[i] = value;
+                        }
+                        break;
+
+                    case 'description':
+                        // Check if the description is a source
+                        if (value[0].length > 0) {
+                            if (value[0].indexOf('Source: ') === 0) {
+                                // We have a source, save the source
+                                m['source'] = value[0].substring(8).trim();
+                            } else {
+                                // Not a source, save to the description field
+                                m[i] = value[0].trim();
+                            }
+                        }
+                        break;
+
+                    default:
+                        // Get the text
+                        if (value[0].length > 0) {
+                            m[i] = value[0].trim();
+                        }
+                }
+            }
+        }
+    }
+    return m;
+}
